654cfc712c0fffe93fdda9e1e2448c27957ad0eb
pertinate <ppertinate@gmail.com>
2022-11-29 22:52:38 -0600

    attempted to play around with listening for inputs to make hotkeys

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
-use std::io;
+use std::fs::{File, OpenOptions};
+use std::os::unix::{
+    fs::OpenOptionsExt,
+    io::{FromRawFd, IntoRawFd, RawFd},
+};
+use std::path::Path;
 
-fn main() {
-    println!("Please type the string: \"Hello World!\"");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    post_main(&input)
+use input::{Libinput, LibinputInterface};
+use libc::{O_RDONLY, O_RDWR, O_WRONLY};
+
+struct Interface;
+
+impl LibinputInterface for Interface {
+    fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<RawFd, i32> {
+        OpenOptions::new()
+            .custom_flags(flags)
+            .read((flags & O_RDONLY != 0) | (flags & O_RDWR != 0))
+            .write((flags & O_WRONLY != 0) | (flags & O_RDWR != 0))
+            .open(path)
+            .map(|file| file.into_raw_fd())
+            .map_err(|err| err.raw_os_error().unwrap())
+    }
+    fn close_restricted(&mut self, fd: RawFd) {
+        unsafe {
+            File::from_raw_fd(fd);
+        }
+    }
 }
 
-fn post_main(param1: &str) {
-    println!("You wrote: {param1}");
+fn main() {
+    let mut input = Libinput::new_with_udev(Interface);
+    input.udev_assign_seat("seat0").unwrap();
+    loop {
+        input.dispatch().unwrap();
+        for event in &mut input {
+            println!("Got event: {:?}", event);
+        }
+    }
 }
